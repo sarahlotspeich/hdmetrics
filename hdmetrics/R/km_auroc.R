fcaeaed6e67e88bc8cdb3678f25686125e859ec5
Abby Foes <agfoes@unc.edu>
2026-04-05 14:51:13 -0400

    remove dependency on survivalROC

--- hdmetrics/R/km_auroc.R
+++ hdmetrics/R/km_auroc.R
@@ -10,17 +10,59 @@
 #' @return numeric AUC value
 #' @export
 #' 
-#' @importFrom survivalROC survivalROC
 #'
-km_auroc = function(evaluate_at, time, event, log_risk, data) {
+km_auroc <- function(evaluate_at, time, event, log_risk, data) {
   
-  auc_val = survivalROC(
-    Stime = data[, time],
-    status = data[, event],
-    marker = data[, log_risk],
-    predict.time = evaluate_at,
-    method = "KM"
-  )$AUC
+  Stime <- data[, time]
+  status <- data[, event]
+  marker <- data[, log_risk]
   
+  km_step <- function(fit, t) {
+    idx <- which(fit$time <= t)
+    if (length(idx) == 0) return(1)
+    fit$surv[max(idx)]
+  }
+  
+  # marginal survival at "evaluate_at"
+  km_fit <- survfit(Surv(Stime, status) ~ 1)
+  S_t <- km_step(km_fit, evaluate_at)
+  F_t <- 1 - S_t
+  
+  # cut points
+  cut_vals <- sort(unique(marker)) # sort the unique log risk values
+  n_cuts <- length(cut_vals)
+  TP <- numeric(n_cuts) # true positive allocation
+  FP <- numeric(n_cute) # false positive allocation
+  
+  # loop over cutpoints  from unique log risk values
+  for (k in seq_len(n_cuts)) {
+    in_group <- (marker > cut_vals[k])
+    p_c <- mean(in_group)
+    
+    if (sum(in_group) == 0 || F_t == 0) {
+      TP[k] <- 0
+      FP[k] <- 0
+      next
+    }
+    
+    km_c <- survfit(Surv(Stime[in_group], status[in_group]) ~ 1)
+    S_t_c <- km_step(km_c, evaluate_at) # survival given log-risk is greater than cut point
+    
+    TP[k] <- (p_c - p_c*S_t_c) / F_t
+    FP[k] <- p_c
+  }
+  
+  # sort cutpoints and add 0, 1
+  FP <- c(0, FP, 1)
+  TP <- c(0, TP, 1)
+  
+  ordFP <- order(FP) # order of FP
+  FP <- FP[ordFP] # reorder FP
+  TP <- TP[ordFP] # reorder in same way as FP to keep paired values together
+  
+  # get AUC via trapezoidal rule
+  auc <- sum(diff(FP) * (head(TP, -1) + tail(TP, -1))/2)
+  
+  return(auc)
   
 }
